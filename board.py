713f394bee13a367e49ab7666cbe7bebffc1ba1f
Emily <reynemil@oregonstate.edu>
2022-09-22 14:54:36 -0700

    3rd draft with better modularization

--- board.py
+++ board.py
@@ -1,47 +1,13 @@
 class Board:
     """Creates a board object for the game Mancala. Initialized in the Mancala class init method."""
 
-    def __init__(self, player1, player2):
-        self._player1_object = player1
-        self._player2_object = player2
-        self._a_turn = True
-        self._first_move = True
+    def __init__(self):
         self._board = {'1': 4, '2': 4, '3': 4, '4': 4, '5': 4, '6': 4, 'A': 0,
                        '7': 4, '8': 4, '9': 4, '10': 4, '11': 4, '12': 4, 'B': 0}
 
     def get_board(self):
         """Returns dictionary that represent the game board"""
         return self._board
-
-    def get_a_turn(self):
-        """returns a_turn bool"""
-        return self._a_turn
-
-    def update_a_turn(self, new_bool):
-        """Updates a_turn to new_bool"""
-        self._a_turn = new_bool
-
-    def get_first_move(self):
-        """Returns first_move data member"""
-        return self._first_move
-
-    def update_first_move(self, new_bool):
-        """Updates first_move to new_bool"""
-        self._first_move = new_bool
-
-    def check_move(self, player, space):
-        """Checks to make sure player is starting move on their side of the board"""
-        space_num = int(space)
-        if player == 'A':
-            if space_num <= 6:
-                return True
-            else:
-                return False
-        if player == 'B':
-            if space_num >= 7:
-                return True
-            else:
-                return False
 
     def check_side(self, player):
         """Checks to see if all spaces on player's side are empty, if so, the player is allowed to
@@ -69,20 +35,6 @@ class Board:
         """Returns true if the space chosen is empty and prompts user to pick another space"""
         if self._board[space] == 0:
             print("This space is empty! Please pick a space with marbles in it. ")
-            return True
-        else:
-            return False
-
-    def game_over(self):
-        """Sets the winner of the game"""
-        if self._board['A'] > self._board['B']:
-            return "A"
-        elif self._board['A'] < self._board['B']:
-            return "B"
-
-    def compare_goals(self):
-        """Returns true if player A has more marbles in their goal than player B and false otherwise"""
-        if self._board['A'] > self._board['B']:
             return True
         else:
             return False
--- mancala2.py
+++ mancala2.py
@@ -2,13 +2,19 @@ from board import Board
 from player import Player
 
 class Mancala:
-    """Defines an instance of the game mancala"""
+    """Defines an instance of the game mancala while initializing the Player class and Board class"""
 
     def __init__(self, name_1, name_2):
         self._playerA = Player("A", name_1)
         self._playerB = Player("B", name_2)
-        self._board = Board(self._playerA, self._playerB)
-        self._winner = None   # will be set to winner
+        self._playerA_route = ['1', '2', '3', '4', '5', '6', 'A', '7', '8', '9', '10', '11', '12']
+        self._playerB_route = ['7', '8', '9', '10', '11', '12', 'B', '1', '2', '3', '4', '5', '6']
+        self._board_object = Board()
+        self._winner = None         # will be set to winner
+        self._a_turn = True         # describes if it's player A's turn. Used to keep track of whose turn it is
+        self._first_move = True     # makes sure a player's starting move is on their own side
+        self._game_over = False     # if game is still in session, will be set to false
+
         print("                                     Welcome to Mancala! ")
         print("  ")
         print("     This is a two-player game. The game board has 12 spaces numbered 1 through 12 and two goal spots")
@@ -25,5 +31,219 @@ class Mancala:
         print("move from the other player's side of the board. The game is over when their are no marbles left in any of")
         print("the spaces. To make a move, use the move() function with the space as a string argument.")
         print("For example: game1._playerA.move('1').")
-        self._playerA.update_board_object(self._board)
-        self._playerB.update_board_object(self._board)
+
+    def get_winner(self):
+        """Return's winner of the game as player object"""
+        return self._winner
+
+    def move(self, player, space):
+        """player chooses which space they will pick up marbles from"""
+        board = self._board_object.get_board()
+        if self.is_game_over():
+            return
+
+        if self._board_object.is_empty(space):  # makes sure player chooses a space with marbles
+            return
+
+        if self.wrong_player(player):
+            return
+
+        if self._board_object.check_side(player):  # check to see if side is empty
+            pass
+        elif self._first_move:
+            if self.check_move(player, space):  # check to see if player makes a legal move
+                pass
+            else:
+                print("Please pick a starting space on your side of the board!")
+                return
+
+        route = self.set_route(player)
+
+        num_marbles = board[space]  # how many spaces the player can move
+        board[space] = 0  # pick up all the marbles in initial space, so it's set to zero
+        route_position = route.index(space)  # index of route list
+        key = space
+
+        while num_marbles != 0:
+            if route_position == 12:
+                route_position -= 12
+                key = route[route_position]
+                board[key] += 1
+                num_marbles -= 1
+            else:
+                route_position += 1  # move forward one on route list
+                key = route[route_position]
+                self.check_points(key, player)
+                board[key] += 1
+                num_marbles -= 1
+
+        if self.next_turn(player, key):
+            return
+
+        if self.ends_in_goal(key):
+            return
+
+        self._first_move = False
+        self.move(player, key)
+
+    def check_points(self, key, player):
+        """Adds 1 point to player's data member self._points"""
+        if key == "A" or key == 'B':
+            if player == 'A':
+                self._playerA.update_points()
+            else:
+                self._playerB.update_points()
+
+    def set_route(self, player):
+        """Sets player route data member"""
+        if player == 'A':
+            return self._playerA_route
+        else:
+            return self._playerB_route
+
+    def ends_in_goal(self, space):
+        """Returns True if the player ended in their points pile. Returns false, otherwise"""
+        board_object = self._board_object
+        board = self._board_object.get_board()
+        key = space
+
+        if key == 'A' or key == 'B':  # ends in their points pile
+            board_object.print_board()
+            if board['A'] == 24 and board['B'] == 24:
+                print("Score: A =", board['A'], "B =", board["B"])
+                print("IT'S A TIE!")
+                self._game_over = True
+                return True
+            elif board['A'] + board['B'] == 48:
+                self._winner = self.set_winner()
+                print("Score: A =", board['A'], "B =", board["B"])
+                print("PLAYER ", self._winner, " WINS!")
+                self._game_over = True
+                return True
+            else:
+                print("You landed in your own points pile! You go again!")
+                self._first_move = True
+                return True
+        else:
+            return False
+
+    def next_turn(self, player, space):
+        """Returns true if the current turn is over, and it's now the other player's turn. Returns false, otherwise"""
+        board_object = self._board_object
+        board = self._board_object.get_board()
+        key = space
+
+        if board[key] == 1 and key != "A" and key != 'B':  # num_marbles is zero and they ended in a space with zero marbles
+            board_object.print_board()
+            if player == "A":
+                print("You landed in an empty space. Player B's turn!")
+                self._first_move = True
+                self._a_turn = False
+            else:
+                print("You landed in an empty space. Player A's turn!")
+                self._first_move = True
+                self._a_turn = True
+            return True
+        else:
+            return False
+
+    def wrong_player(self, player):
+        """Returns True if wrong player is taking a turn, and False if it's the correct player"""
+        if self._a_turn and player == 'A':  # this section makes sure the correct player is taking a turn
+            return False
+        elif self._a_turn and player == "B":
+            print("It is player A's turn!")
+            return True
+        elif self._a_turn == False and player == 'B':
+            return False
+        else:
+            print("It is player B's turn!")
+            return True
+
+    def check_move(self, player, space):
+        """Checks to make sure player is starting move on their side of the board"""
+        space_num = int(space)
+        if player == 'A':
+            if space_num <= 6:
+                return True
+            else:
+                return False
+        if player == 'B':
+            if space_num >= 7:
+                return True
+            else:
+                return False
+
+    def set_winner(self):
+        """Sets the winner of the game"""
+        board = self._board_object.get_board()
+
+        if board['A'] > board['B']:
+            return "A"
+        elif board['A'] < board['B']:
+            return "B"
+
+    def is_game_over(self):
+        """Returns True if this game of mancala is over"""
+        if self._game_over == True:
+            print("Please start a new game of Mancala.")
+            return True
+        else:
+            return False
+"""
+game1 = Mancala("Emily", "Danny")
+game1.move('A', '3')
+game1.move('A', '2')
+game1.move('B', '11')
+game1.move("A", "1")
+game1.move("B", "12")
+game1.move("B", "10")
+game1.move("B", "12")
+game1.move("B", "7")
+game1.move("A", "6")
+game1.move("A", "3")
+game1.move("A", "4")
+game1.move("A", "2")
+game1.move("A", "1")
+game1.move("A", "6")
+game1.move("A", "4")
+game1.move("A", "3")
+game1.move("A", "1")
+game1.move("B", "12")
+game1.move("B", "8")
+game1.move("A", "2")
+game1.move("B", "9")
+game1.move("A", "6")
+game1.move("A", "3")
+game1.move("A", "5")
+game1.move("B", "8")
+game1.move("B", "10")
+game1.move("A", "5")
+game1.move("B", "11")
+game1.move("B", "12")
+game1.move("A", "1")
+game1.move("A", "6")
+game1.move("A", "5")
+game1.move("A", "6")
+game1.move("A", "4")
+game1.move("B", "8")
+game1.move("A", "2")
+game1.move("B", "10")
+game1.move("B", "12")
+game1.move("B", "4")
+game1.move("A", "6")
+game1.move("B", "8")
+game1.move("A", "3")
+game1.move("B", "10")
+game1.move("B", "12")
+game1.move("B", "4")
+game1.move("A", "5")
+game1.move("B", "6")
+game1.move("A", "7")
+game1.move("B", "8")
+game1.move("A", "9")
+game1.move("B", "10")
+game1.move("A", "11")
+game1.move("B", "12")
+game1.move("B", "3")
+"""
